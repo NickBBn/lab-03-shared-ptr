ff946384b55500b16d5953a633c8bf861699a2fa
NickBBn <nickgeo.winner@gmail.com>
2020-12-01 20:46:21 +0300

    fix sigsegv

--- tests/test.cpp
+++ tests/test.cpp
@@ -54,13 +54,17 @@ TEST(ConstructorTest, Rvalue) {
   //int value = 23;
   int *regularPtr1 = new int (23);
   //int *regularPtr2 = new int (99);
+  std::cout << "SharedPtr<int> intShared1(regularPtr1);" << std::endl;
   SharedPtr<int> intShared1(regularPtr1);
+  std::cout << "SharedPtr<int> intShared2(intShared1);" << std::endl;
   SharedPtr<int> intShared2(intShared1);
+  std::cout << "SharedPtr<int> intShared3(std::move(intShared1));" << std::endl;
   SharedPtr<int> intShared3(std::move(intShared1));
+  std::cout << "finished" << std::endl;
   //SharedPtr<int> intShared2(returnObject());
   //SharedPtr<int> intShared2 = returnObject(regularPtr2);
-  int* ptr = intShared2.get();
-  *ptr = 40;
+  //int* ptr = intShared2.get();
+  //*ptr = 40;
   EXPECT_EQ(intShared3.get(), regularPtr1);
   EXPECT_EQ(intShared3.useCount(), 2);
 }
